2aa90ccaaed6a6e926f39c295b083666e8466576
Alvaro Calace <alvarocalace@hotmail.com>
2018-04-12 16:56:47 -0300

    update

---
+++ 2/29.scm
@@ -0,0 +1,60 @@
+;a
+
+(define (make-mobile left right)
+ (list left right))
+
+(define (make-branch length structure)
+ (list length structure))
+
+(define (left-branch mobile)
+ (car mobile))
+
+(define (right-branch mobile)
+ (cadr mobile))
+
+(define (branch-length branch)
+ (car branch))
+
+(define (branch-structure branch)
+ (cadr branch))
+
+(define (mobile? x)
+ (pair? x))
+
+;b
+
+(define (branch-weight branch)
+ (if (mobile? (branch-structure branch))
+     (total-weight (branch-structure branch))
+     (branch-structure branch)))
+
+(define (total-weight mobile)
+ (+ (branch-weight (left-branch mobile))
+    (branch-weight (right-branch mobile))))
+
+;c
+
+(define (torque branch)
+ (* (branch-length branch) (branch-weight branch)))
+
+(define (branch-balanced branch)
+ (if (mobile? (branch-structure branch))
+     (mobile-balanced (branch-structure branch))
+     true))
+
+(define (mobile-balanced mobile)
+ (and (branch-balanced (left-branch mobile))
+      (branch-balanced (right-branch mobile))
+      (= (torque (left-branch mobile))
+         (torque (right-branch mobile)))))
+
+;d
+
+;only need to change the selectors to use cdr instead of cadr
+
+;test
+
+(define x
+ (make-mobile
+  (make-branch 1 (make-mobile (make-branch 1 1) (make-branch 2 2)))
+  (make-branch 2 3)))
